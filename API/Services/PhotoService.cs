a35c867f7b8553c0851c05eddc77262878a2825c
Channrith Moul <channrith.moul@gmail.com>
2024-10-05 23:54:20 +0700

    implemented upload functionality

--- API/Services/PhotoService.cs
+++ API/Services/PhotoService.cs
@@ -1,4 +1,3 @@
-using System;
 using API.Helpers;
 using API.Interfaces;
 using CloudinaryDotNet;
@@ -9,42 +8,42 @@ namespace API.Services;
 
 public class PhotoService : IPhotoService
 {
-    private readonly Cloudinary _cloudinary;
+  private readonly Cloudinary _cloudinary;
 
-    // Initializes the Cloudinary instance
-    public PhotoService(IOptions<CloudinarySettings> config)
+  // Initializes the Cloudinary instance
+  public PhotoService(IOptions<CloudinarySettings> config)
+  {
+    // Setting up the Cloudinary account
+    var account = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
+    _cloudinary = new Cloudinary(account);
+  }
+
+  // Method to upload a photo to Cloudinary asynchronously
+  // Takes an IFormFile as input and returns an ImageUploadResult
+  public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+  {
+    var uploadResult = new ImageUploadResult();
+
+    // Check if the file has content before proceeding with the upload
+    if (file.Length > 0)
     {
-        // Setting up the Cloudinary account
-        var account = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
-        _cloudinary = new Cloudinary(account);
+      using var stream = file.OpenReadStream();
+      var uploadParams = new ImageUploadParams
+      {
+        File = new FileDescription(file.FileName, stream),
+        Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
+        Folder = "profile-pictures"
+      };
+      uploadResult = await _cloudinary.UploadAsync(uploadParams);
     }
 
-    // Method to upload a photo to Cloudinary asynchronously
-    // Takes an IFormFile as input and returns an ImageUploadResult
-    public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
-    {
-        var uploadResult = new ImageUploadResult();
+    return uploadResult;
+  }
 
-        // Check if the file has content before proceeding with the upload
-        if (file.Length > 0)
-        {
-            using var stream = file.OpenReadStream();
-            var uploadParams = new ImageUploadParams
-            {
-                File = new FileDescription(file.FileName, stream),
-                Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
-                Folder = "profile-pictures"
-            };
-            uploadResult = await _cloudinary.UploadAsync(uploadParams);
-        }
+  public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+  {
+    var deleteParams = new DeletionParams(publicId);
 
-        return uploadResult;
-    }
-
-    public async Task<DeletionResult> DeletePhotoAsync(string publicId)
-    {
-        var deleteParams = new DeletionParams(publicId);
-
-        return await _cloudinary.DestroyAsync(deleteParams);
-    }
+    return await _cloudinary.DestroyAsync(deleteParams);
+  }
 }
